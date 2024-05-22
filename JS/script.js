ad96b4907aa34693ff2ef2f4faa9e6b4c99ea7ef
Sandro Silva <sandrosilvadepaula.usa@gmail.com>
2024-05-22 18:50:56 -0400

    3

--- JS/script.js
+++ JS/script.js
@@ -24,7 +24,7 @@ const loop = setInterval( () => {
     
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
-        mario.src = '/JS_Mario/images/game-over.png';
+        mario.src = 'images/game-over.png';
         mario.style.width= '60px';
         mario.style.marginLeft = '50px';
         clearInterval(loop);
