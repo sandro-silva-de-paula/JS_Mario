743f020d1eb25401ba3a1c1d0c009de4fc015912
Sandro Silva <sandrosilvadepaula.usa@gmail.com>
2024-05-22 18:18:16 -0400

    .....

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
+
+
+const jump= () =>{
+    mario.classList.add('jump');
+
+    setTimeout( () => {
+         mario.classList.remove('jump');}, 700);
+
+   
+
+}
+
+const loop = setInterval( () => {
+    const pipePosition = pipe.offsetLeft;
+    const cloudsPosition = clouds.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+   
+    if (pipePosition <= 85 &&  pipePosition >0 && marioPosition<=60){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+    
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src = '/images/game-over.png';
+        mario.style.width= '60px';
+        mario.style.marginLeft = '50px';
+        clearInterval(loop);
+        
+        
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
+
+    }
+    
+}, 10);
+
+
+document.addEventListener('keydown',jump);
