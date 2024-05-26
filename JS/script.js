2ffa4c607bd07b536475b1f169d59da3ddc8b69c
Sandro Silva <sandrosilvadepaula.usa@gmail.com>
2024-05-26 01:29:17 -0400

    melhorando o score

--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,24 @@ const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
 const dvgo = document.querySelector('.div-go');
 const score = document.querySelector('.you-score');
+const list_records = [    
+    {a: 'Priscila Lopes', b: 72000 },
+    {a: 'Joao Gabriel Rego',b: 22000 },
+    {a: 'Renato Castro', b: 42000 },
+    {a: 'Sandro Silva',b: 5000 },
+];
+
+const create_score= (text) => {
+    let score = document.createElement("li");
+    score.innerHTML = text;
+    document.getElementById("myScore").appendChild(score);
+
+  }
+  
+ 
 
 let points = 0
+
 const jump= () =>{
     mario.classList.add('jump');
     setTimeout( () => {
@@ -35,10 +51,22 @@ const loop = setInterval( () => {
 
         clouds.style.animation = 'none';
         clouds.style.left = `${cloudsPosition}px`;
-        //on div-go
-        dvgo.style.display = 'flex'
 
-        score.textContent= 'Your Score: '+`${points*1000}`
+        //on div-go
+        dvgo.style.display = 'flex';
+
+        //add your score a lista e indexar
+        
+        list_records.push( {a: 'Your Score' ,b: points*1000});
+        let list_records_sorted = 
+        list_records.sort(function(a_, b_){return a_.b - b_.b});
+        list_records_sorted=list_records_sorted.toReversed();
+
+
+        for (let i=0; i< list_records_sorted.length; i++){
+            create_score( (i+1) +'° - '+list_records_sorted[i].a +' : ' +list_records_sorted[i].b);
+        };
+
         points=0;
 
         clearInterval(loop);
